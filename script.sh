86ab334305b50b471785cc4da483bcdf12e62a9c
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-05-09 19:20:29 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,11 @@
-# Escriba su código aquí
+sed 's/;;/;\N;/g' data.csv > out.1
+sed 's/;N/;\N/g' out.1 > out.2
+sed 's/,/./g' out.2 > out.3
+sed 's/;/,/g' out.3 > out.4
+sed 's/,N,/,\\N,/g' out.4 > out.5
+sed 's/\//-/g' out.5 > out.6
+sed 's/[a-z]/\U&/g' out.6 > out.7
+sed 's/\([0-9][0-9]\)-\([0-9][0-9]\)-\([0-9][0-9]\)/ 20\3-\2-\1/g' out.7 > out.8
+sed 's/, /,\\N/g' out.8 > out.9
+sed -e'/N/d' out.9 > lab-01-linag.csv
+rm out*
